770eebdd311e723131564da86e73eca4fef3fd26
Dolomirr <dolomirr@gmail.com>
2026-06-21 03:55:42 +0200

    add support for different commands other then sudo

--- functions/sudope.fish
+++ functions/sudope.fish
@@ -1,5 +1,11 @@
 function sudope -d "Quickly toggle sudo prefix"
 
+  set -l _sudope_command "sudo"
+  if set -q SUDOPE_COMMAND
+    set _sudope_command $SUDOPE_COMMAND
+  end
+  set -l prefix_len (string length -- "$_sudope_command ")
+
   # Save the current command line and cursor position.
   set -l command_buffer (commandline)
   set -l cursor_position (commandline -C)
@@ -10,7 +16,8 @@ function sudope -d "Quickly toggle sudo prefix"
   end
 
   # Parse the command line (first line only).
-  set -l command_parts (string match -ir '^(\s*)(sudo(\s+|$))?(.*)' $command_buffer[1])
+  set -l escaped_prefix (string escape --style=regex "$_sudope_command")
+  set -l command_parts (string match -ir "^(\s*)($escaped_prefix(\s+|\$))?(.*)" $command_buffer[1])
 
   # Handle multiline commands with extra care.
   set -l command_lines_count (count $command_buffer)
@@ -22,11 +29,11 @@ function sudope -d "Quickly toggle sudo prefix"
       # No "sudo".
 
       # Add "sudo" to the beginning of the command, after any leading whitespace (if present).
-      commandline -r (string join \n (string join '' $command_parts[2] 'sudo ' $command_parts[3]) $command_rest)
+      commandline -r (string join \n (string join '' $command_parts[2] "$_sudope_command " $command_parts[3]) $command_rest)
 
       # Push the cursor position ahead if necessary (+5 for 'sudo ').
       test $cursor_position -ge (string length -- "$command_parts[2]")
-        and set cursor_position (math $cursor_position+5)
+        and set cursor_position (math $cursor_position+$prefix_len)
 
       # Place the cursor where it was (or where it should be).
       commandline -C $cursor_position
